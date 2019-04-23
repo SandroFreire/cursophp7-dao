b0649f9cc98e61625d7a05bd2eb4b6d359357ed6
Sandro Silveira Freire <Sandro.Freire@correios.com.br>
2019-04-23 09:35:00 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
+//Carrega um usuário
+//$usuarios  = new Usuario();
+//$usuarios->loadbyId(4);
+//echo $usuarios;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("user");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
 $usuario  = new Usuario();
-
-$usuario->loadbyId(4);
-
+$usuario->login("user","12345");
 echo $usuario;
+//echo json_encode($search);
 
  ?>
